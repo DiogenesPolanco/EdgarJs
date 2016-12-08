4b78ebc00f458cac7db2e21e58d22e373b777dc0
Dany Paredes <danywalls4@gmail.com>
2016-12-08 01:58:25 +0100

    :lipstick: clean code and dependecies
    Cleanup each code and dependencies.

--- app.js
+++ app.js
@@ -1,65 +1,112 @@
 var EdgarJs = {
-    load(key,value){
-        if(localStorage.getItem(key)){
-            return localStorage.getItem(key);
+
+
+    Session: {
+
+        load(key, value) {
+            if (localStorage.getItem(key)) {
+                return localStorage.getItem(key);
+            }
+            console.log("no existe");
+
+        },
+        save(key, value) {
+            if (!localStorage.getItem(key)) {
+                localStorage.setItem(key, value)
+                return;
+            }
+            console.log("ya existe")
+
+        },
+        saveJSON(key, value) {
+            if (!sessionStorage.getItem(key)) {
+
+                sessionStorage.setItem(key, JSON.stringify(value));
+                return;
+            }
+
+
+        },
+        loadJSON(key) {
+            if (sessionStorage.getItem(key)) {
+                return JSON.parse(sessionStorage.getItem(key));
+            }
+            return "no existe este key"
+        },
+        remove(key) {
+            if (sessionStorage.getItem(key)) {
+                sessionStorage.removeItem(key)
+            }
+        },
+        removeAll() {
+            sessionStorage.clear();
         }
-        console.log("no existe");
-        
+
     },
-    save(key,value){
-        if(!localStorage.getItem(key)){
-            localStorage.setItem(key,value)
-            return;
+    Local: {
+        load(key, value) {
+            if (localStorage.getItem(key)) {
+                return localStorage.getItem(key);
+            }
+            console.log("no existe");
+
+        },
+        save(key, value) {
+            if (!localStorage.getItem(key)) {
+                localStorage.setItem(key, value)
+                return;
+            }
+            console.log("ya existe")
+
+        },
+        saveJSON(key, value) {
+            if (!localStorage.getItem(key)) {
+
+                localStorage.setItem(key, JSON.stringify(value));
+                return;
+            }
+            console.log("ya existe")
+
+        },
+        loadJSON(key) {
+            if (localStorage.getItem(key)) {
+                return JSON.parse(localStorage.getItem(key));
+            }
+            return "no existe este key"
+        },
+        remove(key) {
+            if (localStorage.getItem(key)) {
+                localStorage.removeItem(key)
+            }
+        },
+        removeAll() {
+            localStorage.clear();
         }
-        console.log("ya existe")
-        
-    },
-    saveJSON(key,value){
-        if(!localStorage.getItem(key)){
-        
-            localStorage.setItem(key,  JSON.stringify(value));
-            return;
-        }
-        console.log("ya existe")
-        
-    },
-    loadJSON(key){
-         if(localStorage.getItem(key)){
-            return JSON.parse(localStorage.getItem(key));
-        }
-        return "no existe este key"
-    },
-    remove(key){
-        if(localStorage.getItem(key)){
-            localStorage.removeItem(key)
-        }
-    },
-    removeAll(){
-        localStorage.clear();
+
     }
 
 
 
 }
-function startup(){
-   
+function startup() {
 
-    $(".load").on("click",function(){
-        var source   = $("#entry-template").html();
+
+    $(".load").on("click", function () {
+        var source = $("#entry-template").html();
         var template = Handlebars.compile(source);
-        var context = EdgarJs.loadJSON("prueba");
-        var html    = template(context);
+        var context = EdgarJs.Local.loadJSON("prueba");
+        var html = template(context);
         $("#app").html(html);
     })
 
-  $(".save").on("click",function(){
-      var bindobject = testObject = {'title': 'edgar','body':'lalalalala'};
+    $(".save").on("click", function () {
+        var bindobject = testObject = { 'title': 'edgar', 'body': 'lalalalala' };
 
-     EdgarJs.saveJSON("prueba",bindobject);
- })
+        EdgarJs.Local.saveJSON("prueba", bindobject);
+    })
 
- $(".delete").on("click",function(){
-     EdgarJs.removeAll();
- })
+    $(".delete").on("click", function () {
+        EdgarJs.Local.removeAll();
+    })
 }
 window.onload = startup;
